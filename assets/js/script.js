875593b23db3e397cd49bdfefd2f215349d63177
EmpireAntz <thebigboognish@gmail.com>
2023-10-21 17:54:02 -0700

    made the input display in the correct text box when saved and will continue to display even when the page is refreshed or another input is made. I also made sure the past present and future classed were being added to the texbox aread correctly. Most of the main functionality of the the project is finished. It just needs some polishing and refining and it needs quality comments and a README added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,22 +3,43 @@
 // in the html.
 
 $(function () {
+  //Displays the date and time 
   $('#currentDay').text(dayjs().format('MM/D/YYYY, h:mm A'))
+  
+  updateTimeBlockStyle()
+
+  loadTextAreaValues() 
+  
   var saveBtn = $(".saveBtn")
+  
   saveBtn.on('click', function() {
     var hour = $(this).parent().attr('id')
     var userInput = $(this).siblings('.description').val()
-    console.log(this)
-    console.log("saved")
-    console.log(hour)
+    
     localStorage.setItem(hour, userInput)
-    var savedUserInput = localStorage.getItem(hour)
-    console.log("this is what should save: " + savedUserInput)
+  })
+
+  function loadTextAreaValues() {
+    $(".time-block").each(function() {
+        var hour = $(this).attr('id')
+        var savedValue = localStorage.getItem(hour)
+        
+        if (savedValue) {
+          $(this).find('.description').val(savedValue)
+        }
+    })
+  }
+
+  function updateTimeBlockStyle() {
+    var currentHour = dayjs().hour()
     var timeBlock = $(".time-block")
+  
     timeBlock.each(function() {
       var calendarHourText = this.children[0].textContent
       var calendarHour
+      
       console.log(calendarHourText)
+      
       if (calendarHourText.includes('AM')) {
         calendarHour = parseInt(calendarHourText.replace('AM','').trim())
       }
@@ -28,29 +49,27 @@ $(function () {
           calendarHour += 12
         }
       }
+      
       console.log(calendarHour)
-      var currentHour = dayjs().hour()
-      var textArea = $('.description')
       console.log(currentHour)
+      
+      var textArea = $(this).find('.description')
+      
       if (calendarHour < currentHour) {
-        console.log('past hour')
-       console.log(textArea)
-       textArea.addClass('past')
+        textArea.addClass('past')
       }
       else if (calendarHour === currentHour) {
-        console.log('present hour')
         textArea.addClass('present')
       }
       else {
-        console.log('future hour')
         textArea.addClass('future')
       }
     })
-  })
+  }
 })
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
+// TODO: Add a listener for click events on the save button. This code should
+// use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
